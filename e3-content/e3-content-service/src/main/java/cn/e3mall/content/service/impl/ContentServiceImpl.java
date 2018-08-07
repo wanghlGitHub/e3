7033655e6cc72f03b46e17b4157d928d3394b1f4
heliang.wang <wangheliang@bj-sjzy.com>
2018-08-07 23:45:51 +0800

    添加 redis

--- e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
+++ e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
@@ -3,10 +3,14 @@ package cn.e3mall.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import cn.e3mall.common.jedis.JedisClient;
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.JsonUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import cn.e3mall.common.utils.E3Result;
@@ -28,6 +32,11 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+
+	@Value("${CONTENT_LIST}")
+	private String CONTENT_LIST;
 
 	@Override
 	public EasyUIDataGridResult getContentList(long categoryId, int page, int rows) {
@@ -54,6 +63,8 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(new Date());
 		//插入到数据库
 		contentMapper.insert(content);
+		//缓存同步,删除缓存中对应的数据。
+		jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
 		return E3Result.ok();
 	}
 
@@ -67,12 +78,30 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public List<TbContent> getContentListByCid(long cid) {
+		//查询缓存
+		try {
+			//如果缓存中有直接响应结果
+			String json = jedisClient.hget(CONTENT_LIST, cid + "");
+			if (StringUtils.isNotBlank(json)) {
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//如果没有查询数据库
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		//设置查询条件
 		criteria.andCategoryIdEqualTo(cid);
 		//执行查询
 		List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+		//把结果添加到缓存
+		try {
+			jedisClient.hset(CONTENT_LIST, cid + "", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return list;
 	}
 
@@ -89,6 +118,8 @@ public class ContentServiceImpl implements ContentService {
 		//更新内容
 		contentMapper.updateByPrimaryKey(content);
 		//返回结果
+		//缓存同步,删除缓存中对应的数据。
+		jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
 		return E3Result.ok();
 	}
 
@@ -100,6 +131,8 @@ public class ContentServiceImpl implements ContentService {
 			contentMapper.deleteByPrimaryKey(Long.valueOf(id));
 		}
 		//返回结果
+//		//缓存同步,删除缓存中对应的数据。
+//		jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
 		return E3Result.ok();
 	}
 
